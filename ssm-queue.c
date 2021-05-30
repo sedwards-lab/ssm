23a94266d3f7f608a28e1002a62e8207d914cd84
j-hui <j-hui@cs.columbia.edu>
2021-05-30 00:11:39 -0400

    Fixed queue implementation

--- ssm-queue.c
+++ ssm-queue.c
@@ -7,8 +7,9 @@
  * this compilation unit.
  */
 #include "ssm-queue.h"
-#include "ssm-act.h" /* for definition of struct act */
-#include "ssm-sv.h"  /* for definition of struct sv */
+#include "ssm-act.h"        /* for definition of struct act */
+#include "ssm-queue-test.h" /* for testing */
+#include "ssm-sv.h"         /* for definition of struct sv */
 
 /*** Priority queue logic {{{ */
 
@@ -20,10 +21,10 @@ static inline void enqueue(int (*compar)(void *, void *, void *), void *ctx,
                            void (*set)(void *, idx_t, void *), void *queue,
                            size_t queue_len, void *to_insert) {
   /*
-   * NOTE: queue_len is already 1 greater than the number of elements, so its value is
-   * 2 greater than the last element in a 0-indexed scheme. However, in idx_t's
-   * 1-indexed scheme, it is just 1 past the last element---where we want the
-   * hole to be.
+   * NOTE: queue_len is already 1 greater than the number of elements, so its
+   * value is 2 greater than the last element in a 0-indexed scheme. However, in
+   * idx_t's 1-indexed scheme, it is just 1 past the last element---where we
+   * want the hole to be.
    */
   idx_t hole = queue_len + 1 - QUEUE_HEAD;
   for (; hole > QUEUE_HEAD && compar(to_insert, get(queue, hole >> 1), ctx) < 0;
@@ -41,11 +42,36 @@ static inline void fill_hole(int (*compar)(void *, void *, void *), void *ctx,
                              void *(*get)(void *, idx_t),
                              void (*set)(void *, idx_t, void *), void *queue,
                              size_t queue_len, idx_t hole, void *to_insert) {
+
+  /* If to_insert is less than the parent of the hole */
+  if (hole == QUEUE_HEAD || compar(get(queue, hole >> 1), to_insert, ctx) < 0)
+    /* We may need to move hole down, so we check its children */
+    goto percolate_down;
+
+  /* Otherwise, move hole up until reaching somewhere suitable for insertion */
+  for (;;) {
+    idx_t parent = hole >> 1;
+
+    if (compar(get(queue, parent), to_insert, ctx) <= 0)
+      break;
+
+    set(queue, hole, get(queue, parent));
+    hole = parent;
+
+    /* No more parents to check; insert at root of heap */
+    if (hole == QUEUE_HEAD)
+      break;
+  }
+
+  set(queue, hole, to_insert);
+  return;
+
+percolate_down:
   for (;;) {
     /* Find earlier of hole's two children */
     idx_t child = hole << 1; /* Left child */
 
-    if (queue_len <= child)
+    if (queue_len < child)
       /* Reached the bottom of minheap */
       break;
 
@@ -54,8 +80,9 @@ static inline void fill_hole(int (*compar)(void *, void *, void *), void *ctx,
         compar(get(queue, child + 1), get(queue, child), ctx) < 0)
       child++; /* Right child is earlier */
 
-    /* Is to_insert earlier than both children? */
-    if (compar(to_insert, get(queue, child), ctx) < 0)
+    /* Is to_insert a suitable parent for (less than/equal to) both children? */
+    if (compar(to_insert, get(queue, child), ctx) <= 0)
+      /* to_insert is a suitable parent for (leq) both children */
       break;
 
     /* If not, swap earlier child up (push hole down), and descend */
@@ -97,7 +124,8 @@ void enqueue_event(struct sv **event_queue, size_t *queue_len,
           &to_insert);
 }
 
-void dequeue_event(struct sv **event_queue, size_t *queue_len, idx_t to_dequeue) {
+void dequeue_event(struct sv **event_queue, size_t *queue_len,
+                   idx_t to_dequeue) {
   /*
    * We don't need to create a separate copy of the tail of the queue because
    * we decrement the queue_size before we call fill_hole, leaving tail beyond
@@ -180,5 +208,64 @@ idx_t index_of_act(struct act **act_queue, size_t *queue_len,
 }
 
 /*** Act queue }}} */
+
+/*** long queue (for testing) {{{ */
+/* For convenience, the 0th element of these arrays stores the length */
+
+static int compar_test(void *lp, void *rp, void *ctx) {
+  long l = *(long *)lp, r = *(long *)rp;
+
+  if (l < r)
+    return -1;
+  else if (l == r)
+    return 0;
+  else
+    return 1;
+}
+
+static void *get_test(void *queue, idx_t idx) {
+  long *q = (long *)queue;
+  return &q[idx];
+}
+
+static void set_test(void *queue, idx_t idx, void *val) {
+  long *q = (long *)queue, *v = (long *)val;
+  q[idx] = *v;
+}
+
+void enqueue_test(long *long_queue, long to_insert) {
+  long *queue_len = &long_queue[QUEUE_LEN];
+  enqueue(compar_test, NULL, get_test, set_test, long_queue, ++*queue_len,
+          &to_insert);
+}
+
+void dequeue_test(long *long_queue, idx_t to_dequeue) {
+  /*
+   * We don't need to create a separate copy of the tail of the queue because
+   * we decrement the queue_size before we call fill_hole, leaving tail beyon
+   * the queue.
+   */
+  long *queue_len = &long_queue[QUEUE_LEN];
+  long *to_insert = &long_queue[QUEUE_HEAD + --*queue_len];
+  fill_hole(compar_test, NULL, get_test, set_test, long_queue, *queue_len,
+            to_dequeue, to_insert);
+}
+
+void requeue_test(long *long_queue, idx_t to_requeue) {
+  long *queue_len = &long_queue[QUEUE_LEN];
+  long to_insert = long_queue[to_requeue];
+  fill_hole(compar_test, NULL, get_test, set_test, long_queue, *queue_len,
+            to_requeue, &to_insert);
+}
+
+idx_t index_of_test(long *long_queue, long to_find) {
+  long *queue_len = &long_queue[QUEUE_LEN];
+  for (idx_t idx = QUEUE_HEAD; idx < *queue_len; idx++)
+    if (long_queue[idx] == to_find)
+      return idx;
+  return 0;
+}
+
+/*** long queue }}} */
 
 /* vim: set ts=2 sw=2 tw=80 et foldmethod=marker :*/
