003c998459426c049dee791358787f3bed031c86
Hans Montero <hjm2133@columbia.edu>
2021-06-06 23:49:45 -0400

    Add doc for simulation time driver

--- simulation-time.c
+++ simulation-time.c
@@ -1,6 +1,14 @@
+/**
+ * Implementation of a simulation time driver, which doesn't actually suspend
+ * program executation between events, but rather jumps the runtime to the next
+ * event time.
+ */
+
 #include "ssm-queue.h"
 #include "ssm-sv.h"
 #include "ssm-runtime.h"
+
+/*** Time driver API, exposed via time-driver.h {{{ ***/
 
 void initialize_time_driver() {
   // No-op
@@ -14,3 +22,5 @@ ssm_time_t timestep() {
   set_now(next);
   return next;
 }
+
+/*** Time driver API, exposed via time-driver.h }}} ***/
