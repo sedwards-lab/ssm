1d6dc92f9f774fda49eeeefe3b72d15eeccd10ee
j-hui <j-hui@cs.columbia.edu>
2021-06-08 22:53:11 -0400

    Fix how build system handles libraries, with LIBS
    Also fix runtime Build.mk, which was just missing some dependencies

--- Build.mk
+++ Build.mk
@@ -4,12 +4,13 @@
 ##
 
 CPPFLAGS += -I $(RUNTIMEDIR)/include
-LDFLAGS += -lpeng
+LDLIBS += -lpeng
 
 vpath %.c $(RUNTIMEDIR)/src
 
-RUNTIMESRC := peng-scheduler.c peng-int.c peng-bool.c
+RUNTIMESRC := peng-scheduler.c peng-bool.c peng-int64.c peng-int.c peng-uint8.c
 
 SRCS += $(RUNTIMESRC)
+LIBS += libpeng.a
 
 libpeng.a : libpeng.a($(RUNTIMESRC:%.c=%.o))
