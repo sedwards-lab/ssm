fd5cc536e24d54de11a7e5ee8639d0062900541f
j-hui <j-hui@cs.columbia.edu>
2021-07-20 00:24:12 -0400

    Fix unsched bug when hole is beyond the queue after event removal

--- src/ssm-scheduler.c
+++ src/ssm-scheduler.c
@@ -297,8 +297,8 @@ void ssm_unschedule(ssm_sv_t *var)
     q_idx_t hole = find_queued_event(var);
     var->later_time = SSM_NEVER;
     ssm_sv_t *moved_var = event_queue[event_queue_len--];
-    if (event_queue_len > 0) {
-      // Only percolate if there events left in the queue.
+    if (hole < SSM_QUEUE_HEAD + event_queue_len) {
+      // Percolate if removal led to hole in the queue.
       if (hole == SSM_QUEUE_HEAD ||
           event_queue[hole >> 1]->later_time < moved_var->later_time)
         event_queue_percolate_down(hole, moved_var);
