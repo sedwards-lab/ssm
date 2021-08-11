42b5b845d16d6a23fa09cc9f43e6e1c795d00691
j-hui <j-hui@cs.columbia.edu>
2021-08-11 13:36:18 -0400

    Shaved off some work from the input handler
    Hitting 37kHz before dropping packets (previously 34kHz).

--- examples/freqcounter-mpsc-platformio-zephyr/include/ssm-platform.h
+++ examples/freqcounter-mpsc-platformio-zephyr/include/ssm-platform.h
@@ -2,15 +2,14 @@
 #define _SSM_PLATFORM_H
 
 #include <drivers/gpio.h>
-#include <ssm.h>
-#include <sys/__assert.h>
 #include <logging/log.h>
 #include <logging/log_ctrl.h>
-#include <sys/ring_buffer.h>
+#include <ssm.h>
+#include <sys/__assert.h>
 #include <sys/mpsc_pbuf.h>
+#include <sys/ring_buffer.h>
 
-extern struct ssm_act *(*ssm_entry_point)(struct ssm_act *, ssm_priority_t,
-                                          ssm_depth_t);
+extern int ssm_program_initialize(void);
 
 #define SSM_DEBUG_ASSERT(cond, ...)                                            \
   if (!(cond))                                                                 \
@@ -23,80 +22,49 @@ extern struct ssm_act *(*ssm_entry_point)(struct ssm_act *, ssm_priority_t,
 
 /* "Disable" trace-related stuff. */
 #define SSM_DEBUG_TRACE(...)                                                   \
-  do; while (0)
+  do                                                                           \
+    ;                                                                          \
+  while (0)
 #define SSM_DEBUG_MICROTICK()                                                  \
-  do; while (0)
+  do                                                                           \
+    ;                                                                          \
+  while (0)
+
+typedef struct {
+  uint8_t type;
+  uint8_t index;
+} input_info;
 
 /** The SSM timer device, declared here for ease of access. */
 extern const struct device *ssm_timer_dev;
 
-/**** I/O ****/
+/** Static I/O devices */
+extern ssm_event_t *const sw0;
+extern ssm_event_t *const sw1;
+extern ssm_bool_t *const led0;
 
-/** My recreation of Zephyr 2.6's struct dt_spec */
-typedef struct {
-  const char *label;
-  gpio_pin_t pin;
-  gpio_flags_t flags;
-} gpio_device_t;
-
-/** Create a gpio_device_t from a device id/alias */
-#define DT_GPIO_DEV(id)                                                        \
-  (gpio_device_t) {                                                            \
-    .label = DT_GPIO_LABEL(DT_ALIAS(id), gpios),                               \
-    .pin = DT_GPIO_PIN(DT_ALIAS(id), gpios),                                   \
-    .flags = DT_GPIO_FLAGS(DT_ALIAS(id), gpios)                                \
-  }
-
-/**
- * Contains data used in for binding input to (event) variables.
- * Must be persisted for as long that input is bound.
- */
-typedef struct {
-  struct gpio_callback cb;
-  ssm_event_t *sv;
-  const struct device *port;
-} ssm_input_event_t;
-
-/** Bind sv to input dev, storing the data in in. */
-void bind_input_handler(ssm_input_event_t *in, ssm_event_t *sv,
-                        gpio_device_t dev);
-
-/** Unbind the sv in in. */
-void unbind_input_handler(ssm_input_event_t *in);
-
-/** Bind an SV (and a kill switch) to an output device. */
-ssm_act_t *enter_out_handler(ssm_act_t *parent, ssm_priority_t priority,
-                             ssm_depth_t depth, ssm_bool_t *ref_led,
-                             ssm_event_t *ref_off, gpio_device_t dev);
+extern int initialize_static_input_device(ssm_sv_t *sv);
+extern ssm_sv_t *lookup_input_device(input_info *input);
+extern ssm_act_t *initialize_static_output_device(ssm_act_t *parent,
+                                                  ssm_priority_t priority,
+                                                  ssm_depth_t depth,
+                                                  ssm_sv_t *sv);
 
 /**** Zephyr event queue ****/
 
+enum { SSM_EVENT_T };
+
 typedef struct {
   MPSC_PBUF_HDR;
-  uint32_t input_tag: 32 - MPSC_PBUF_HDR_BITS; // tells us metadata about SV
-  /* TODO: reduce size */
-  /* uint8_t idx; // gives us pointer to SV */
-  /* uint16_t unused; */
-  ssm_time_t time;
-  ssm_sv_t *sv;
-  // TODO: value
+  uint16_t unused : 16 - MPSC_PBUF_HDR_BITS; // 14 bits of unused space here.
+  input_info input;
+  uint32_t tick;
+  uint32_t mtk0;
+  uint32_t mtk1;
 } ssm_input_packet_t;
 
 extern struct k_sem tick_sem;
 extern struct mpsc_pbuf_buffer input_pbuf;
 extern uint32_t dropped;
-
-/*/1** Types of events from the environment *1/ */
-/*typedef enum { SSM_TIMEOUT, SSM_EXT_INPUT } ssm_event_type_t; */
-
-/*/1** An event from the environment, e.g., a timeout *1/ */
-/*typedef struct { */
-/*  ssm_event_type_t type; */
-/*  ssm_time_t time; */
-/*  ssm_event_t *sv; // TODO: handle values */
-/*} ssm_env_event_t; */
-
-/** The queue */
-/* extern struct k_msgq ssm_env_queue; */
 
 #endif /* _SSM_PLATFORM_H */
--- examples/freqcounter-mpsc-platformio-zephyr/src/timer64.c
+++ examples/freqcounter-mpsc-platformio-zephyr/src/timer64.c
@@ -5,52 +5,16 @@
 
 LOG_MODULE_REGISTER(timer64);
 
-#define TIMER64_LOGICAL_BITS 31
-#define TIMER64_TOTAL_BITS (TIMER64_LOGICAL_BITS + 1)
-
-#define TIMER64_LO_PARITY_BIT(ctr) (!!((ctr) & (0x1u << TIMER64_LOGICAL_BITS)))
-#define TIMER64_HI_PARITY_BIT(mtk) ((mtk)&0x1u)
-
-#define TIMER64_LO_MSB(ctr) (!!((ctr) & (0x1u << (TIMER64_LOGICAL_BITS - 1))))
-
-#define TIMER64_HI_SHIFT(hi) (((uint64_t)(hi)) << TIMER64_LOGICAL_BITS)
-#define TIMER64_LO_MASK(lo) ((lo) & ((0x1u << TIMER64_LOGICAL_BITS) - 1))
-
-#define TIMER64_COMBINE(hi, lo) (TIMER64_HI_SHIFT(hi) + TIMER64_LO_MASK(lo))
-
-#define TIMER64_TOP                                                            \
-  ((0x1u << (TIMER64_TOTAL_BITS - 1)) |                                        \
-   ((0x1u << (TIMER64_TOTAL_BITS - 1)) - 1))
-#define TIMER64_MID (0x1u << TIMER64_LOGICAL_BITS)
-#define TIMER64_GUARD (TIMER64_TOP / 2)
-
 enum { TIMER64_MID_ALARM, TIMER64_USER_ALARM };
 
-static volatile uint32_t macroticks;
+volatile uint32_t macroticks;
 
 uint64_t timer64_read(const struct device *dev) {
 
   uint32_t ctr, mtk0, mtk1;
 
-  mtk0 = macroticks;
-
-  compiler_barrier();
-
-  counter_get_value(dev, &ctr);
-
-  compiler_barrier();
-
-  mtk1 = macroticks;
-
-  if (mtk0 == mtk1)
-    if (TIMER64_LO_PARITY_BIT(ctr) == TIMER64_HI_PARITY_BIT(mtk0))
-      return TIMER64_COMBINE(mtk0, ctr);
-    else
-      return TIMER64_COMBINE(mtk0 + 1, ctr);
-  else if (TIMER64_LO_MSB(ctr))
-    return TIMER64_COMBINE(mtk0, ctr);
-  else
-    return TIMER64_COMBINE(mtk1, ctr);
+  TIMER64_READ(dev, &ctr, &mtk0, &mtk1);
+  return TIMER64_CALC(ctr, mtk0, mtk1);
 }
 
 static inline void incr_macroticks(const struct device *dev) { macroticks++; }
